2b336b99d89d7c6363290e3417e5f7e28033ecac
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-11 13:43:45 +0300

    Update main.c

--- test/main.c
+++ test/main.c
@@ -12,7 +12,7 @@ int main(void)
 	int len;
 	int len2;
 	unsigned int ui;
-	void *adr;
+	void *addr;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
