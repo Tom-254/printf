c2e1d28c5f1ca6c2c1bb7d17303492da37c53009
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-11 14:41:05 +0300

    Update getFunc_and_buffer_functions.c

--- test/getFunc_and_buffer_functions.c
+++ test/getFunc_and_buffer_functions.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * get_func - returns needed function
+ * @i: identifier for function
+ * Return: Pointer to needed function
+ */
+char* (*get_func(char i))(va_list)
+{
+	int k = 0;
+
+	print keys[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'d', print_d},
+		{'i', print_d},
+		{'b', itob},
+		{'R', rot13},
+		{'r', rev_string},
+		{'o', itoOctal},
+		{'\0', NULL}
+	};
+
+	while (keys[k].id != '\0')
+	{
+		if (keys[k].id == i)
+			return (keys[k].func);
+		k++;
+	}
+	return (NULL);
+}
+
+/**
  * create_buffer - creates buffer to hold string until it's ready for print
  * Return: pointer to buffer created
  */
