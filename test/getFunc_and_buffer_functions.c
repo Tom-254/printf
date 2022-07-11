705931fbab5f4f3339f008b431d183ac1ad1c9dc
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-11 14:38:10 +0300

    Update getFunc_and_buffer_functions.c

---
+++ test/getFunc_and_buffer_functions.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_buffer - creates buffer to hold string until it's ready for print
+ * Return: pointer to buffer created
+ */
+char *create_buffer(void)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (NULL);
+	return (buffer);
+}
+
+/**
+ * write_buffer - prints buffer, then frees it and frees va_list
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+void write_buffer(char *buffer, int len, va_list list)
+{
+	char *buff;
+
+	buff = realloc(buffer, len); /* realloc to correct size */
+	write(1, buff, len); /* print */
+
+	free(buff); va_end(list);
+}
