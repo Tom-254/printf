be2ea6d5009ad9a7eede3ed8591626c127551660
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-12 21:41:13 +0300

    Update print.c

--- print.c
+++ print.c
@@ -108,18 +108,36 @@ int _printf(const char *format, ...)
  */
 int main(void)
 {
-	_printf("\n\n\nHere's some examples of what you could do with this custom_printf function!\n\n\n");
-	sleep(1);
-	_printf("\nPrinting Strings, Characters, and Numbers...... %s %c%drld\n\n", "Hello", 'W', 0);
-	sleep(1);
-	_printf("Printing Reverse...... %r \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Binary (base 2)...... %b \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Octal (base 8)...... %o \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Rot13 (encrypt)...... %R \n\n", "Hello");
-	sleep(1);
-	_printf("\n\n             = )                  \n\n\n");
-	return (0);
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
